aa80bea4b3828e3891a4b4289bb72949b304c5b7
vaishna19 <77892336+vaishna19@users.noreply.github.com>
2021-05-26 16:20:00 +0530

    jas

--- App.js
+++ App.js
@@ -31,35 +31,19 @@ const User = db.define('User',
   {
   // Other model options go here
   });
- console.log(User === sequelize.models.User); // true 
+ //console.log(User === sequelize.models.User); // true 
 const User1 = db.define('User1',
  {
   // Model attributes are defined here
   firstName: {
     type: DataTypes.STRING,
     allowNull: false
-  },
-  school: {
-    type: DataTypes.STRING
-    // allowNull defaults to true
-    },
-  address: {
-    type: DataTypes.INTEGER            
-    // allowNull defaults to true
-    },
-  Place: {
-    type: DataTypes.STRING            
-    // allowNull defaults to true
-    },
+  }
 },
   {
   // Other model options go here
   });
-console.log(User1 === sequelize.models.User1); // true
-// db.authenticate()
-//   .then(() => {
-//     console.log('connection success')
-//   })
+
 module.exports - {
   db
 }
